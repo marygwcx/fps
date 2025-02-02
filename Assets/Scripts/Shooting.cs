7ac01af3e9df457bbe359e8a320923eabac99df9
marygwcx <marijaagejevaite@gmail.com>
2025-02-02 13:02:08 +0200

    movement and camera

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float speed = 15f;
+    public float lifeTime = 2f;
+    void Start()
+    {
+                
+    }
+
+    void Update()
+    {
+        
+    }
+}
